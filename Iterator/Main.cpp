54aca641174cb90ce30412f5ffbb8e6a4ba61e14
thaiqi <haiqi.tang@apulis.com>
2023-07-31 15:59:15 +0800

    feat(Iterator): 2 - add LinkedList Company

--- Iterator/Main.cpp
+++ Iterator/Main.cpp
@@ -2,6 +2,7 @@
 // Created by Mac on 2023/7/31.
 //
 #include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
@@ -11,8 +12,9 @@ public:
     string name;
     int age;
     string info;
+    Staff *next = nullptr;
 
-    Staff(const string &name, int age, const string &info) : name(name), age(age), info(info) {}
+    Staff(string name, int age, string info) : name(std::move(name)), age(age), info(std::move(info)) {}
 };
 
 class Company {
@@ -25,11 +27,32 @@ public:
     }
 };
 
+class Company2 {
+public:
+    Staff *head = new Staff("", 0, "");
+
+    void addStaff(const string &name, int age, const string &info) const {
+        Staff *p = head;
+        while (p->next != nullptr) {
+            p = p->next;
+        }
+        p->next = new Staff(name, age, info);
+    }
+};
+
 int main() {
     Company company;
     company.addStaff("name1", 1, "test1");
     company.addStaff("name2", 2, "test2");
     for (const Staff &staff: company.staffVec) {
         printf("name:%s,age:%d,info:%s\n", staff.name.c_str(), staff.age, staff.info.c_str());
+    }
+    Company2 company2;
+    company2.addStaff("name1", 1, "test1");
+    company2.addStaff("name2", 2, "test2");
+    Staff *p = company2.head;
+    while (p->next != nullptr) {
+        p = p->next;
+        printf("name:%s,age:%d,info:%s\n", p->name.c_str(), p->age, p->info.c_str());
     }
 }
