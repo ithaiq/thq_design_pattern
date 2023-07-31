6849a3bec61b58dcdb24c6d5a7232717cfa60135
thaiqi <haiqi.tang@apulis.com>
2023-07-31 15:13:03 +0800

    feat(Observer): 2 - add UserMessageObserver

--- Observer/Main.cpp
+++ Observer/Main.cpp
@@ -3,29 +3,60 @@
 //
 // Created by Mac on 2023/7/31.
 //
+class Observer {
+public:
+    virtual void update() = 0;
+};
+
+
 class UserWallet {
 public:
     float balance = 0.0;
+    Observer *observer = nullptr;
 
-    float deposit(float p) {
-        return balance += p;
+    void deposit(float p) {
+        balance += p;
     };
 
-    float consume(float p) {
-        return balance -= p;
+    void consume(float p) {
+        balance -= p;
+        observer->update();
     };
 
     float getBalance() {
         return balance;
     }
+
+    void attachObserver(Observer *_observer) {
+        observer = _observer;
+    }
 };
+
+class UserMessageObserver : public Observer {
+public:
+    UserWallet *userWallet = nullptr;
+
+    explicit UserMessageObserver(UserWallet *userWallet) : userWallet(userWallet) {
+        userWallet->attachObserver(this);
+    }
+
+    void update() override {
+        printf("%f notify\n", userWallet->getBalance());
+        if (userWallet->getBalance() <= 3.0) {
+            printf("balance low\n");
+        }
+    }
+};
+
 
 int main() {
     UserWallet wallet;
+    UserMessageObserver observer(&wallet);
+//    wallet.attachObserver(&observer);
     wallet.deposit(100);
     for (int i = 0; i < 100; i++) {
         wallet.consume(1.0);
-        printf("%f\n", wallet.getBalance());
+//        printf("%f notify\n", wallet.getBalance());
     }
     return 0;
 }
