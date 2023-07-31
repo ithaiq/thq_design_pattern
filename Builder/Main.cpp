9f28a4b97c9a87d2b79e571aab704d6b5a199cfc
thaiqi <haiqi.tang@apulis.com>
2023-07-31 17:19:40 +0800

    feat(Builder): 2 - add Set Function

--- Builder/Main.cpp
+++ Builder/Main.cpp
@@ -2,7 +2,6 @@
 // Created by Mac on 2023/7/31.
 //
 #include <string>
-#include <iostream>
 
 using namespace std;
 
@@ -13,11 +12,38 @@ public:
     string mRedisAddr;
     string mRedisDb;
 
+    SystemConfig() {};
+
     SystemConfig(const string &mMysqlAddr, const string &mMysqlDb, const string &mRedisAddr, const string &mRedisDb)
-            : mMysqlAddr(mMysqlAddr), mMysqlDb(mMysqlDb), mRedisAddr(mRedisAddr), mRedisDb(mRedisDb) {}
+            : mMysqlAddr(mMysqlAddr), mMysqlDb(mMysqlDb), mRedisAddr(mRedisAddr), mRedisDb(mRedisDb) {};
+
+    void SetRedis(const string &redisAddr, const string &redisDb) {
+        mRedisAddr = redisAddr;
+        mRedisDb = redisDb;
+    }
+
+    void SetMysql(const string &mysqlAddr, const string &mysqlDb) {
+        mMysqlAddr = mysqlAddr;
+        mMysqlDb = mysqlDb;
+    }
 };
 
 int main() {
     SystemConfig config("test", "test2", "test3", "test4");
+    printf("config mysqlAddr:%s,mMysqlDb:%s,mRedisAddr:%s,mRedisDb:%s\n",
+           config.mMysqlAddr.c_str(),
+           config.mMysqlDb.c_str(),
+           config.mRedisAddr.c_str(),
+           config.mRedisDb.c_str()
+    );
+    SystemConfig config2;
+    config2.SetMysql("test11", "test12");
+    config2.SetRedis("test21", "test22");
+    printf("config2 mysqlAddr:%s,mMysqlDb:%s,mRedisAddr:%s,mRedisDb:%s\n",
+           config2.mMysqlAddr.c_str(),
+           config2.mMysqlDb.c_str(),
+           config2.mRedisAddr.c_str(),
+           config2.mRedisDb.c_str()
+    );
     return 0;
 }
