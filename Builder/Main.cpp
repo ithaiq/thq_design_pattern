27493c987de2f8c94406f05c143ee7a0b9034d89
thaiqi <haiqi.tang@apulis.com>
2023-07-31 18:18:11 +0800

    feat(Builder): 3 - add builder Function

--- Builder/Main.cpp
+++ Builder/Main.cpp
@@ -17,14 +17,35 @@ public:
     SystemConfig(const string &mMysqlAddr, const string &mMysqlDb, const string &mRedisAddr, const string &mRedisDb)
             : mMysqlAddr(mMysqlAddr), mMysqlDb(mMysqlDb), mRedisAddr(mRedisAddr), mRedisDb(mRedisDb) {};
 
+
+};
+
+class SystemConfigBuilder {
+public:
+    SystemConfig cfg;
+
+    [[nodiscard]] SystemConfig getSystemConfig() const {
+        return cfg;
+    }
+
     void SetRedis(const string &redisAddr, const string &redisDb) {
-        mRedisAddr = redisAddr;
-        mRedisDb = redisDb;
+        cfg.mRedisAddr = redisAddr;
+        cfg.mRedisDb = redisDb;
     }
 
     void SetMysql(const string &mysqlAddr, const string &mysqlDb) {
-        mMysqlAddr = mysqlAddr;
-        mMysqlDb = mysqlDb;
+        cfg.mMysqlAddr = mysqlAddr;
+        cfg.mMysqlDb = mysqlDb;
+    }
+};
+
+class System {
+private:
+    SystemConfigBuilder builder;
+public:
+    SystemConfig buildSystemConfig() {
+        builder.SetRedis("r1", "r2");
+        return builder.getSystemConfig();
     }
 };
 
@@ -36,9 +57,10 @@ int main() {
            config.mRedisAddr.c_str(),
            config.mRedisDb.c_str()
     );
-    SystemConfig config2;
-    config2.SetMysql("test11", "test12");
-    config2.SetRedis("test21", "test22");
+    SystemConfigBuilder builder;
+    builder.SetMysql("test11", "test12");
+    builder.SetRedis("test21", "test22");
+    SystemConfig config2 = builder.getSystemConfig();
     printf("config2 mysqlAddr:%s,mMysqlDb:%s,mRedisAddr:%s,mRedisDb:%s\n",
            config2.mMysqlAddr.c_str(),
            config2.mMysqlDb.c_str(),
