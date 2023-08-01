ff160d00a55c2ee16a7b22dd404d5c61a80ede5d
thaiqi <haiqi.tang@apulis.com>
2023-08-01 19:08:03 +0800

    feat(Adapter): 2 - add Adapter

--- Adapter/Main.cpp
+++ Adapter/Main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 class Profit {
 public:
-    float getProfit() {
+    virtual float getProfit() {
         return 1.0;
     }
 };
@@ -15,8 +15,16 @@ public:
 class USDProfit {
 public:
     float getUSDProfit(const string &name, const string &addr) {
-        return 2.0;
+        return 2;
     };
+};
+
+class USDAdapter : public Profit, public USDProfit {
+public:
+    float getProfit() {
+        float usdProfitNum = getUSDProfit("abc", "123");
+        return usdProfitNum * 7.0;;
+    }
 };
 
 int main() {
@@ -26,4 +34,12 @@ int main() {
     USDProfit usdProfit;
     float usdProfitNum = usdProfit.getUSDProfit("abc", "123");
     usdProfitNum = usdProfitNum * 7.0;
+    printf("usdProfitNum:%f\n", usdProfitNum);
+
+    USDAdapter usdAdapter;
+    printf("usdProfitNum1:%f\n", usdAdapter.getProfit());
+
+    Profit *usdAdapter2 = new USDAdapter();
+    printf("usdProfitNum2:%f\n", usdAdapter2->getProfit());
+    delete usdAdapter2;
 }
